fc12f76706af2616b6e2f6e283932bc76c8f8d65
charmruber <1912758611@qq.com>
2015-07-03 00:32:09 +0800

    steping...

--- bin/gulp.js
+++ bin/gulp.js
@@ -4,7 +4,9 @@ var Liftoff = require('liftoff');
 var interpret = require('interpret');
 var v8flags = require('v8flags');
 var completion = require('../lib/completion');
-
+var argv = require('minimist')(process.argv.slice(2));
+var gutil = require('gulp-util');
+var chalk = require('chalk');
 
 // Set env var for ORIGINAL cwd
 // before anything touches it
@@ -19,3 +21,28 @@ var cli = new Liftoff({
 
 // Exit with 0 or 1
 var failed = false;
+process.once("exit", function(code) {
+    if(code === 0 && failed) {
+        process.exit(1);
+    }
+});
+
+// Parse those args m8
+var cliPackage = require('../package');
+var versionFlag = argv.v || argv.version;
+var tasksFlag = argv.T || argv.tasks;
+var tasks = argv._;
+var toRun = tasks.length ? tasks : ['default'];
+
+// this is a hold-over until we have a better logging system
+// with log levels
+var simpleTasksFlag = argv['tasks-simple'];
+var shouldLog = !argv.silent && !simpleTasksFlag;
+
+if(!shouldLog) {
+    gutil.log = function() {};
+}
+
+cli.on('require', function(name) {
+    gutil.log('Requiring external module', chalk.magenta(name));
+});
